763328c43f21fd12a3667ef818165da63d5d1238
B. Endres <endres@systopia.de>
2019-08-29 11:59:57 +0200

    [#62] added TODO marker

--- CRM/Sqltasks/Utils.php
+++ CRM/Sqltasks/Utils.php
@@ -6,6 +6,7 @@
 class CRM_Sqltasks_Utils {
 
   public static function isSegmentationInstalled() {
+    // TODO: cache? but be aware, unit tests need this uncached atm
     return civicrm_api3('Extension', 'getcount', [
       'full_name' => 'de.systopia.segmentation',
       'status'    => 'installed',
