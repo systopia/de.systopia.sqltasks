c1a766b2890fa67276471e003e55b875b2d5e136
Agiliway Group, Inc <github@agiliway.com>
2020-03-23 14:43:30 +0200

    Use service instead of functions in scope.

--- ang/sqlTaskConfigurator.js
+++ ang/sqlTaskConfigurator.js
@@ -20,19 +20,25 @@
     }
   ]);
 
-  angular
-    .module(moduleName)
-    .controller("sqlTaskConfiguratorCtrl", function($scope, $location, taskId) {
+  angular.module(moduleName).service('loaderService', function() {
+    this.executionBlock = {
+      'isLoaded' : false,
+      'currentLoadedComponents' : 0,
+      'componentsNumber' : 2,
+    };
+    this.updateExecutionBlock = function() {
+      this.executionBlock.currentLoadedComponents = this.executionBlock.currentLoadedComponents  + 1;
+      if (this.executionBlock.currentLoadedComponents >= this.executionBlock.componentsNumber) {
+        this.executionBlock.isLoaded = true;
+      }
+    };
+    this.isExecutionBlockLoaded = function() {
+      return this.executionBlock.isLoaded;
+    };
+  });
+
+  angular.module(moduleName).controller("sqlTaskConfiguratorCtrl", function($scope, $location, taskId, loaderService) {
       $scope.ts = CRM.ts();
-      $scope.executionBlock = {
-        'isLoaded' : false,
-        'currentLoadedComponents' : 0,
-        'componentsNumber' : 2,
-      };
-      $scope.handleBlockLoading = handleBlockLoading;
-      $scope.isExecutionBlockLoaded = function() {
-        return $scope.executionBlock.isLoaded;
-      };
       $scope.taskOptions = {
         scheduled: ""
       };
@@ -43,6 +49,9 @@
         scheduled_day: "1",
         scheduled_hour: "0",
         scheduled_minute: "0"
+      };
+      $scope.isExecutionBlockLoaded = function () {
+        return loaderService.isExecutionBlockLoaded();
       };
       $scope.taskId = taskId;
 
@@ -132,7 +141,7 @@
           });
         });
         $scope.permissionsData = permissionsData;
-        $scope.executionBlock = $scope.handleBlockLoading($scope.executionBlock);
+        loaderService.updateExecutionBlock();
       });
 
       $scope.onSchedulingOptionChange = function(params) {
@@ -241,7 +250,7 @@
             scheduled_day: 1
           });
         }
-        $scope.executionBlock = $scope.handleBlockLoading($scope.executionBlock);
+        loaderService.updateExecutionBlock();
         $scope.$apply();
       });
 
@@ -800,15 +809,6 @@
       file: file
     });
     return false;
-  }
-
-  function handleBlockLoading(block) {
-    block.currentLoadedComponents = block.currentLoadedComponents  + 1;
-    if (block.currentLoadedComponents >= block.componentsNumber) {
-      block.isLoaded = true;
-    }
-
-    return block;
   }
 
   angular.module(moduleName).directive("successHandler", function() {
