ae0092057cd6cb41a304027db928474440024ea4
Manuel Flandorfer <mflandor@greenpeace.org>
2020-11-30 15:30:31 +0100

    GP-13519 Define global setting 'sqltasks_default_template'

--- settings/sqltasks.setting.php
+++ settings/sqltasks.setting.php
@@ -11,4 +11,14 @@ return [
     'is_contact'  => 0,
     'description' => ts('Array of global tokens for SQL Tasks which may be used using the {config.token_name} syntax.'),
   ],
+  'sqltasks_default_template' => [
+    'name'        => 'sqltasks_default_template',
+    'type'        => 'String',
+    'default'     => null,
+    'html_type'   => 'text',
+    'title'       => ts('SQL Tasks default template'),
+    'is_domain'   => 1,
+    'is_contact'  => 0,
+    'description' => ts('Default configuration (ID) for new SQL Tasks.'),
+  ],
 ];
