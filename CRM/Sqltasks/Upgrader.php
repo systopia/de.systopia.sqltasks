a2752a64d6ecde01a63f442577d86f555e7c5498
Manuel Flandorfer <mflandor@greenpeace.org>
2024-01-10 16:38:39 +0100

    GP-44570 Upgrade extension

--- CRM/Sqltasks/Upgrader.php
+++ CRM/Sqltasks/Upgrader.php
@@ -18,7 +18,7 @@ use CRM_Sqltasks_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Sqltasks_Upgrader extends CRM_Sqltasks_Upgrader_Base {
+class CRM_Sqltasks_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
